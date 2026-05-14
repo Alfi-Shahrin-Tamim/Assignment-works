5f52f53d21901232595ac597ade25f971bb22b50
Alfi-Shahrin-Tamim <bsse1816@iit.du.ac.bd>
2026-05-15 01:40:39 +0600

    Create rev_pyramid.c
    reversed pyramid

---
+++ rev_pyramid.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+
+int main()
+
+{
+
+    int n,i,j,x;
+
+    printf("Give a value for n: ");
+
+    scanf("%d", &n);    
+
+    for(i=1;i<=n;i++){
+      for(x=1;x<i;x++){
+        printf(" ");
+      }
+      for(j=1;j<=n-i+1;j++){
+          printf(" *");
+      }
+      printf("\n");
+    }
+}  
