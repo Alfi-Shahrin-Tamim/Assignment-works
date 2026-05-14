ced5e991ae3936a69f7f8938746ade54d02d541d
Alfi-Shahrin-Tamim <bsse1816@iit.du.ac.bd>
2026-05-15 01:44:59 +0600

    Create diamond.c
    double pyramid basically

---
+++ diamond.c
@@ -0,0 +1,31 @@
+ #include <stdio.h>
+
+int main()
+
+{
+
+    int n,i,j,x;
+
+    printf("Give a value for n: ");
+    scanf("%d", &n);
+    for(i=1;i<=n;i++){
+      for(j=1;j<=n;j++){
+        if(j >= n-i+1){
+        printf(" *");
+        }
+        else{
+          printf(" ");
+        }
+      }
+      printf("\n");
+    }    
+    for(i=2;i<=n;i++){
+      for(x=1;x<i;x++){
+        printf(" ");
+      }
+      for(j=1;j<=n-i+1;j++){
+          printf(" *");
+      }
+      printf("\n");
+    }
+}  
