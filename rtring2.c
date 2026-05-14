e4ee5025fb878dae4c8c36d4029cf86c497efb63
Alfi-Shahrin-Tamim <bsse1816@iit.du.ac.bd>
2026-05-14 23:07:25 +0600

    Add files via upload

---
+++ rtring2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Give a value for n: ");
+    scanf("%d", &n);
+    for(i=1;i<=n;i++){
+      for(j=n-i+1;j>=1;j--){
+        printf("* ");
+      }
+      printf("\n");
+    }
+}   
