8d1e93f68be7a6e508421f90d47b47c19a682b8c
Alfi-Shahrin-Tamim <bsse1816@iit.du.ac.bd>
2026-05-15 14:47:31 +0600

    Update rev_pyramid.c
    reversed pyramid

--- rev_pyramid.c
+++ rev_pyramid.c
@@ -1,15 +1,10 @@
  #include <stdio.h>
 
 int main()
-
 {
-
     int n,i,j,x;
-
     printf("Give a value for n: ");
-
     scanf("%d", &n);    
-
     for(i=1;i<=n;i++){
       for(x=1;x<i;x++){
         printf(" ");
