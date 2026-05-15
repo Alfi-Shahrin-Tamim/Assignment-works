ab14ec868ee3d9228f51726189d7259a1ce4c773
Alfi-Shahrin-Tamim <bsse1816@iit.du.ac.bd>
2026-05-15 11:13:57 +0600

    Update btrfly.c
    butterfly shaped program

--- btrfly.c
+++ btrfly.c
@@ -4,20 +4,64 @@ int main()
 
 {
 
-    int n,i,j,x,y;
+    int n,i,j,x,y,lines,stars;
 
     printf("Number of lines: ");
     scanf("%d", &n);
-    x = n /2;
-    y = n - x;
+    lines = 2 * (n-2) + 1;
+    stars = n - 2;
+    x = (lines - 1) / 2;
+    y = (lines - 1) - x;
     for(i=1;i<=x;i++){
-        printf("*****\\ /*****\n");
-        printf("-----\\ /-----\n");
+        if(i % 2 == 0){
+            for(j=1;j<=stars;j++){
+                printf("-");
+            }
+            printf("\\ /");
+            for(j=1;j<=stars;j++){
+                printf("-");
+            }
+            printf("\n");
+        }
+        else{
+            for(j=1;j<=stars;j++){
+                printf("*");
+            }
+            printf("\\ /");
+            for(j=1;j<=stars;j++){
+                printf("*");
+            }
+            printf("\n");
+        }
+ 
     }
-    printf("      @\n");
+    for(j=1;j<=stars + 1;j++){
+        printf(" ");
+    }
+    printf("@\n");
     for(i=1;i<=y;i++){
-        printf("*****/ \\*****\n");
-        printf("-----/ \\-----\n");
-    }    
+        if(i % 2 == 0){
+            for(j=1;j<=stars;j++){
+                printf("-");
+            }
+            printf("\\ /");
+            for(j=1;j<=stars;j++){
+                printf("-");
+            }
+            printf("\n");
+        }
+        else{
+            for(j=1;j<=stars;j++){
+                printf("*");
+            }
+            printf("\\ /");
+            for(j=1;j<=stars;j++){
+                printf("*");
+            }
+            printf("\n");
+        }
+ 
+    }
+        
     
 }  
